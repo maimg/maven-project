15d2bd4571a1b4cee89bba244df0f3f25c8954e6
maimg <maimg15@gmail.com>
2019-09-26 15:14:58 +0200

    Update missing

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,12 @@
+
 /**
- * Info about this package doing something for package-info.java file.
+ * Domain classes used to produce the JSON and XML output for the RESTful services. 
+ * <p>
+ * These classes contain the JAXB annotations.
+ *
+ * @since 1.0
+ * @author jwhite
+ * @version 1.1
  */
 package com.example;
 
